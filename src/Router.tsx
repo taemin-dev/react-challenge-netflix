b6f73214246e8d4d6f484fc81b66ef2c7738b59e
ptm119 <133971709+taemin-dev@users.noreply.github.com>
2024-01-28 19:35:27 +0900

    CreateHashRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./routes/Home";
 import Tv from "./routes/Tv";
 import Search from "./routes/Search";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: "/",
