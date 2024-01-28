65133dc5d3ab176314d478ddab9231275b3b7f91
ptm119 <133971709+taemin-dev@users.noreply.github.com>
2024-01-28 16:46:06 +0900

    Basename (netflix)

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,43 +4,46 @@ import Home from "./routes/Home";
 import Tv from "./routes/Tv";
 import Search from "./routes/Search";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-        children: [
-          {
-            path: "movies/:movieId",
-            element: <></>,
-          },
-        ],
-      },
-      {
-        path: "tv",
-        element: <Tv />,
-        children: [
-          {
-            path: ":tvId",
-            element: <></>,
-          },
-        ],
-      },
-      {
-        path: "search",
-        element: <Search />,
-        children: [
-          {
-            path: ":id",
-            element: <></>,
-          },
-        ],
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+          children: [
+            {
+              path: "movies/:movieId",
+              element: <></>,
+            },
+          ],
+        },
+        {
+          path: "tv",
+          element: <Tv />,
+          children: [
+            {
+              path: ":tvId",
+              element: <></>,
+            },
+          ],
+        },
+        {
+          path: "search",
+          element: <Search />,
+          children: [
+            {
+              path: ":id",
+              element: <></>,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  { basename: process.env.PUBLIC_URL }
+);
 
 export default router;
